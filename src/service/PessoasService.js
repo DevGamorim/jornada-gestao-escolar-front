251c9f08bccfa3038ae4e7dcede5372ef906b239
koalaalk <site3@koalaeletroshop.com.br>
2021-10-15 11:24:29 -0300

    Initial commit

---
+++ src/service/PessoasService.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const Pessoas_REST_API_URL = "http://localhost:8080/pessoa"
+
+class PessoasService {
+
+    getPessoas(){
+        axios.get(Pessoas_REST_API_URL)
+    }
+}
+
+export default new PessoasService();
